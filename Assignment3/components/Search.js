366dd68f23e756d695ce32e783b38ba565cc143b
Programming-Sai <saiahprog6@gmail.com>
2024-05-30 09:59:15 +0000

    Adding Search Component

--- Assignment3/components/Search.js
+++ Assignment3/components/Search.js
@@ -1,9 +1,47 @@
-import { View, Text, SafeAreaView, Image} from "react-native";
+import { View, Image, TextInput, TouchableOpacity} from "react-native";
 
 export default function Search() {
   return (
     <View>
-        
+        <View 
+          style={{
+              width:"354px",
+              height:"52px",
+              borderColor: 'black', 
+              display:"flex",
+              justifyContent:"space-between",
+              flexDirection:"row", 
+              marginTop:20}}>
+                <View 
+                    style={{
+                        display:"flex",
+                        justifyContent:"space-between",
+                        alignItems:"center",
+                        flexDirection:"row",
+                        gap:25
+                    }}>
+                        <View style={{
+                            width:"80%",
+                            backgroundColor:"white",
+                            borderRadius:15,
+                            width:"75%",
+                            display:"flex",
+                            alignItems:"center",
+                            flexDirection:"row"
+                         }}>
+                            <Image source={require('../assets/Vector.jpg')} style={{marginLeft:20}}/>
+                            <TextInput placeholder="Search" placeholderTextColor="black" style={{
+                                padding:10,
+                                width:"100%"
+                            }}/>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={{backgroundColor:"red", padding:10, borderRadius:15}}>
+                                <Image source={require("../assets/bx_slider.png")}/>
+                            </TouchableOpacity>
+                        </View>
+                </View>
+              </View>
     </View>
   );
 }
